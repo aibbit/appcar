59f718dfb88db51a3f536072b0096473bfe76765
TongXY <aibbittong@gmail.com>
2022-07-02 16:41:36 +0800

    7/2 完成轨迹回放脚本

--- script/record_play.py
+++ script/record_play.py
@@ -3,57 +3,77 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import draw_car as draw
 import numpy as np
-import math
 
-dt = 0.2  # 时间间隔，单位：s
-L = 2.9  # 车辆轴距，单位：m
 PI = np.pi
 
-#读取csv数据 转换为list
+
+# 读取csv数据 转换为list
 def read_csv(filename):
-    readData = pd.read_csv(filename,header=None)
-    #print(readDataRaw)
+    readData = pd.read_csv(filename, header=None)
+    # print(readDataRaw)
 
-    #获取readData中的第1列,并将此转换为list
-    data_x = readData.iloc[:,0].tolist()
-    data_y = readData.iloc[:,1].tolist()
-    data_yaw = readData.iloc[:,2].tolist()
-    data_steer = readData.iloc[:,3].tolist()
-    data_v = readData.iloc[:,4].tolist()
-    timeData = list(range(1,len(data_x)+1))        #产生横轴坐标
-    #print(data_x)
-    return timeData,data_x,data_y,data_yaw,data_steer,data_v
+    # 获取readData中的第1列,并将此转换为list
+    data_x = readData.iloc[:, 0].tolist()
+    data_y = readData.iloc[:, 1].tolist()
+    data_yaw = readData.iloc[:, 2].tolist()
+    data_steer = readData.iloc[:, 3].tolist()
+    data_v = readData.iloc[:, 4].tolist()
+    timeData = list(range(1, len(data_x) + 1))  # 产生横轴坐标
+    # print(data_x)
+    return timeData, data_x, data_y, data_yaw, data_steer, data_v
 
 
-class VehicleState:
-    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0,steer=0.0):
+class CarState:
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, steer=0.0, v=0.0):
         self.x = x
         self.y = y
         self.yaw = yaw
-        self.v = v
         self.steer = steer
+        self.v = v
 
-    def update(state, a, delta):
-
-        # if delta > PI/6:
-        #     delta = PI/6
-        # if delta < -PI/6:
-        #     delta = -PI/6
-
-        state.x = state.x + state.v * math.cos(state.yaw) * dt
-        state.y = state.y + state.v * math.sin(state.yaw) * dt
-        state.yaw = state.yaw + state.v / L * math.tan(delta) * dt
-        state.v = state.v + a * dt
-        state.steer = delta
+    def update(state, x, y, yaw, steer, v):
+        state.x = x
+        state.y = y
+        state.yaw = state.yaw + yaw
+        state.steer = steer
+        state.v = v
         return state
+
 
 def main():
 
-    T = 200.0  # 最大模拟时间
+    timeData, data_x, data_y, data_yaw, data_steer, data_v = read_csv(
+        "CarState.csv")
 
+    # 设置车辆的初始状态
+    state = CarState(x=data_x[0],
+                     y=data_y[0],
+                     yaw=data_yaw[0],
+                     steer=data_steer[0],
+                     v=data_v[0])
 
+    for index in range(len(timeData)):
 
-    read_csv()
+        state = CarState.update(state, data_x[index], data_y[index],
+                                data_yaw[index], data_steer[index],
+                                data_v[index])
+        cx = data_x[index]
+        cy = data_y[index]
+
+        plt.cla()
+        plt.plot(cx, cy, ".r", label="course")
+        plt.plot(data_x, data_y, "-b", label="trajectory")
+
+        draw.draw_car(state.x, state.y, PI + state.yaw, state.steer)
+
+        plt.axis("equal")
+        plt.grid(True)
+        # plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4] + "  " + "angle:" + str(state.steer* 57.29578)[:4])
+        plt.title("angle:" + str(state.steer * 57.29578)[:4] + "  " + "yaw:" +
+                  str(state.yaw * 57.29578)[:4])
+        plt.pause(0.01)
+    plt.show()
 
 
 if __name__ == '__main__':
